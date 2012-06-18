9c09276ea424abe0ce4b80f001732c2a19dd9269
Emilio López <elopez93@gmail.com>
2012-06-17 21:26:27 -0300

    First commit

---
+++ README.mk
@@ -0,0 +1,12 @@
+Proyecto final de Estructuras de Datos y Algoritmos I
+=====================================================
+
+El proyecto básicamente consiste en la implementación de dos utilidades
+utilizando C, `my-diff` y `my-repo`. La primera debe comparar dos archivos
+recibidos como parámetros e imprimir las diferencias entre ellos, al igual
+que GNU diff sin parámetros extras. Por otro lado, `my-repo` será un
+sistema de control de versiones sencillo y eficiente, que utilizará el
+algoritmo implementado en `my-diff`.
+
+Para una explicación completa de la tarea a realizar, vea `doc/Enunciado.pdf`
+
