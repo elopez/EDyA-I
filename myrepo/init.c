cd4b1d11530710383b654c056bd6a858dc7bed8b
Emilio López <elopez93@gmail.com>
2012-06-19 19:22:37 -0300

    Initialize catalog with the current date

--- myrepo/init.c
+++ myrepo/init.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 
 int myrepo_init(void)
 {
     struct stat st;
+    FILE* fd;
 
     if(stat(".index", &st) == 0)
     {
@@ -17,10 +19,22 @@ int myrepo_init(void)
         return 1;
     }
 
-    if(mkdir(".index", 0770) == -1) {
+    if(mkdir(".index", 0770) == -1 || mkdir(".index/revs", 0770) == -1)
+    {
         fprintf(stderr, "Error creating index.\n");
         return 1;
     }
+
+    fd = fopen(".index/contents", "w");
+
+    if (fd == NULL)
+    {
+        fprintf(stderr, "Error creating catalog.\n");
+        return 1;
+    }
+
+    fprintf(fd, "created=%u\n", (unsigned int) time(NULL));
+    fclose(fd);
 
     fprintf(stdout, "Repository initialized correctly.\n");
 
