45b39ae5eb54aee82a6891d56a39619432dd7717
Emilio López <elopez93@gmail.com>
2012-06-20 16:04:08 -0300

    myrepo: init: make sure we are not inside a repo
    We cannot include a repo inside of another one. This is a explicitely
    requested feature:
    
        Cuando un directorio contenga un directorio .index o _index
        con un catálogo de my-repo, ningún subdirectorio de éste podrá
        contener otro directorio .index o _index y por lo tanto ningún
        subdirectorio deberá contener un repositorio my-repo.
    
    To be done: if the repo directory is not empty, make sure there is
    no repo inside of it on any subdirectory.

--- myrepo/init.c
+++ myrepo/init.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <time.h>
@@ -8,6 +10,8 @@ int myrepo_init(void)
 {
     struct stat st;
     FILE* fd;
+    char *curdir;
+    int i;
 
     if(stat(".index", &st) == 0)
     {
@@ -17,6 +21,32 @@ int myrepo_init(void)
             fprintf(stderr, ".index is already being used, aborting.\n");
 
         return 1;
+    }
+    
+    /* Linux extension magic here */
+    curdir = getcwd(NULL, 0);
+    
+    if (curdir != NULL)
+    {
+        for(i=strlen(curdir)-1; i > 0; i--)
+        {
+            if (curdir[i] == '/')
+            {
+                curdir[i] = '\0';
+                chdir(curdir);            
+                if(stat(".index", &st) == 0 && S_ISDIR(st.st_mode))
+                {
+                    fprintf(stderr, "You are inside a repository. Aborting.\n");
+                    free(curdir);
+                    return 1;
+                }
+                curdir[i] = '/';
+            }
+        }
+        
+        /* restore the full original path */
+        chdir(curdir);
+        free(curdir);
     }
 
     if(mkdir(".index", 0770) == -1 || mkdir(".index/revs", 0770) == -1)
