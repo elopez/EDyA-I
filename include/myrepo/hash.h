76598ba1014629ece2e1e7ca107e5f42884b8533
Emilio López <elopez93@gmail.com>
2012-06-21 21:11:26 -0300

    Basic file hashing functionality

---
+++ include/myrepo/hash.h
@@ -0,0 +1,6 @@
+#ifndef __H_HASH__
+#define __H_HASH__
+
+char* hash_file(char *path);
+
+#endif
---
+++ myrepo/hash.c
@@ -0,0 +1,61 @@
+#include <assert.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <myrepo/hash.h>
+#include <myrepo/sha1.h>
+
+char* hash_file(char *path)
+{
+    blk_SHA_CTX ctx;
+    FILE* file;
+    char buffer[4096];
+    char *hash;
+    void *bhash;
+    size_t len;
+
+    /* path should never be null */
+    assert(path != NULL);
+
+    file = fopen(path, "r");
+    if (file == NULL)
+        return NULL;
+
+    /* get some memory to store the hex and bin representation */
+    hash = (char *) malloc(41 * sizeof(char));
+    if (hash == NULL)
+    {
+        fclose(file);
+        return NULL;
+    }
+
+    bhash = malloc(20 * sizeof(unsigned char));
+    if (bhash == NULL)
+    {
+        fclose(file);
+        free(hash);
+        return NULL;
+    }
+
+    /* initialize */
+    blk_SHA1_Init(&ctx);
+
+    /* feed hash function */
+    while(!feof(file))
+    {
+        len = fread(buffer, sizeof(char), sizeof(buffer)/sizeof(char), file);
+        blk_SHA1_Update(&ctx, buffer, len);
+    }
+
+    /* finalize hash */
+    blk_SHA1_Final((unsigned char *)bhash, &ctx);
+
+    /* generate hex representation. binary hash is *big endian* */
+    #define PIECE(i) (ntohl(*(unsigned int*)(bhash + (i)*sizeof(unsigned int))))
+    sprintf(hash, "%8x%8x%8x%8x%8x", PIECE(0), PIECE(1), PIECE(2),
+        PIECE(3), PIECE(4));
+
+    free(bhash);
+    return hash;
+}
