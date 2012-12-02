b6126d02d7dcabf78c826ec0f8397d7c0ee490db
Emilio López <elopez93@gmail.com>
2012-12-02 02:05:02 -0300

    Clean up, comment and document log functionality

--- include/myrepo/log.h
+++ include/myrepo/log.h
@@ -1,1 +1,11 @@
+#ifndef __H_LOG__
+#define __H_LOG__
+
+/**
+ * Prints the history of the repository, from most recent commit
+ * to the oldest one.
+ * @return 0 on success, other values on error.
+ */
 int myrepo_log(void);
+
+#endif
--- myrepo/log.c
+++ myrepo/log.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <time.h>
 
+#include <shared/pager.h>
+
 #include <myrepo/catalog.h>
 #include <myrepo/commit.h>
 #include <myrepo/log.h>
@@ -16,51 +18,60 @@ int myrepo_log(void)
     char tmpread[255];
     time_t time;
     unsigned int revision;
-    
-    if (catalogpath == NULL)
-    {
+
+    if (catalogpath == NULL) {
         fprintf(stderr, "You are not inside a repository, aborting.\n");
         return 1;
     }
-    
-    revision = commit_latest(catalogpath, 0);
-    
+
+    /* Initialize the pager output and find out what's the latest commit
+     * on the repository */
     fp = pager_init();
-    
-    revpath = (char*) malloc(strlen(catalogpath) + 100); /* TODO */
-    
+    revision = commit_latest(catalogpath, 0);
+
+    revpath = (char *)malloc(strlen(catalogpath) + 100);
+    if (revpath == NULL) {
+        fprintf(stderr, "Error allocating memory for catalog path\n");
+        return 1;
+    }
+
     do {
         sprintf(revpath, "%s/.index/revs/%d.rev", catalogpath, revision);
         frev = fopen(revpath, "r");
-        if (frev == NULL)
-        {
+        if (frev == NULL) {
             fprintf(stderr, "Failed fetching revision file\n");
             return 1;
         }
-        
+
+        /* Find out the corresponding root tree hash and print it */
         do {
             fgets(tmpread, 200, frev);
-		} while (strncmp("hash=", tmpread, 5) != 0);
-        fprintf(fp, "commit\t#%u %s", revision, tmpread+5);
-        
+        } while (strncmp("hash=", tmpread, 5) != 0);
+
+        fprintf(fp, "commit\t#%u %s", revision, tmpread + 5);
+
+        /* Find out the corresponding date */
         do {
             fgets(tmpread, 200, frev);
-		} while (strncmp("date=", tmpread, 5) != 0);
-        time = (time_t) atoi(tmpread+5);
+        } while (strncmp("date=", tmpread, 5) != 0);
+
+        /* Convert timestamp to text and print it */
+        time = (time_t) atoi(tmpread + 5);
         strftime(tmpread, 200, "%c", localtime(&time));
         fprintf(fp, "Date:\t%s\n\n", tmpread);
-        
-        while(!feof(frev) && fgets(tmpread, 200, frev))
+
+        /* Finally print the commit message */
+        while (!feof(frev) && fgets(tmpread, 200, frev))
             fprintf(fp, "\t%s", tmpread);
-        
+
         fprintf(fp, "\n");
-        
+
         fclose(frev);
     } while (--revision != 0);
-    
+
+    /* Close the pager and clean up */
     pager_close(fp);
-    
     free(revpath);
-    
+
     return 0;
 }
