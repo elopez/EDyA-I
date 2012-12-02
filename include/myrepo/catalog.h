696085830c44e6c97287e29a431e767d0538ae50
Emilio López <elopez93@gmail.com>
2012-12-02 18:53:23 -0300

    catalog: cleanup, indent, comment, document and fix small issues

--- include/myrepo/catalog.h
+++ include/myrepo/catalog.h
@@ -1,14 +1,53 @@
-#include "hashtree.h"
+#include <myrepo/hashtree.h>
 
 #ifndef __H_CATALOG__
 #define __H_CATALOG__
 
+/*
+ * Locates the root of a repository, where a catalog is found.
+ * @return the path to the root of the repository, or NULL if you are
+ *         not inside of one or an error occurs.
+ */
 char *catalog_locate(void);
 
-FILE* catalog_open(void);
-int catalog_exists(FILE* fp, const char* file);
-void catalog_add(FILE* fp, const char* file);
-void catalog_remove(FILE* fp, const char* file);
-char* catalog_hash(FILE* fp, HashTreeNode** copytree, int store);
+/*
+ * Opens the current repository catalog
+ * @return the opened catalog, or NULL if you are not inside of
+ *         of a repository.
+ */
+FILE *catalog_open(void);
+
+/*
+ * Check if a specific file is registered on our catalog
+ * @param[in]   fp          the opened catalog
+ * @param[in]   file        the file name/path to check
+ * @return 0 if it is not registered, the line number on the catalog
+ *           file if it is
+ */
+int catalog_exists(FILE * fp, const char *file);
+
+/*
+ * Appends a file name/path to our catalog if it is not registered
+ * already.
+ * @param[in]   fp          the opened catalog
+ * @param[in]   file        the file name/path to add
+ */
+void catalog_add(FILE * fp, const char *file);
+
+/*
+ * Removes a name/path from our catalog if it is registered on it.
+ * @param[in]   fp          the opened catalog
+ * @param[in]   file        the file name/path to add
+ */
+void catalog_remove(FILE * fp, const char *file);
+
+/*
+ * Generates a hash tree from the current catalog
+ * @param[in]   fp          the opened catalog
+ * @param[out]  copytree    a copy of the generated tree
+ * @param[in]   store       whether we should store the tree on a file
+ * @return the root tree hash, or NULL on error.
+ */
+char *catalog_hash(FILE * fp, HashTreeNode ** copytree, int store);
 
 #endif
--- myrepo/catalog.c
+++ myrepo/catalog.c
@@ -4,6 +4,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <assert.h>
 
 #include <shared/cleanup.h>
 #include <shared/salloc.h>
@@ -38,16 +39,13 @@ char *catalog_locate(void)
     origindir = strdup(curdir);
 
     /* visit every parent dir and check for a catalog */
-    for(i=strlen(curdir)-1; i > 0; i--)
-    {
-        if (curdir[i] == '/')
-        {
+    for (i = strlen(curdir) - 1; i > 0; i--) {
+        if (curdir[i] == '/') {
             curdir[i] = '\0';
             chdir(curdir);
 
             if (stat(".index", &st) == 0 && S_ISDIR(st.st_mode) &&
-                stat(".index/contents", &st) == 0 && S_ISREG(st.st_mode))
-            {
+                stat(".index/contents", &st) == 0 && S_ISREG(st.st_mode)) {
                 /* found, return to starting point */
                 chdir(origindir);
                 free(origindir);
@@ -66,17 +64,19 @@ char *catalog_locate(void)
     return NULL;
 }
 
-FILE* catalog_open(void)
+FILE *catalog_open(void)
 {
     char *catalogpath = catalog_locate();
     char *catalog;
-    FILE* fp;
+    FILE *fp;
 
-    if(catalogpath == NULL)
+    /* Are we inside of a repository? */
+    if (catalogpath == NULL)
         return NULL;
 
-    catalog = (char *) smalloc((strlen(catalogpath) +
-        strlen("/.index/contents") + 1) * sizeof(char));
+    /* Open the catalog and return fp */
+    catalog = smalloc((strlen(catalogpath) +
+                       strlen("/.index/contents") + 1) * sizeof(char));
     sprintf(catalog, "%s/.index/contents", catalogpath);
     fp = fopen(catalog, "a+");
     free(catalog);
@@ -84,7 +84,7 @@ FILE* catalog_open(void)
     return fp;
 }
 
-int catalog_exists(FILE* fp, const char* file)
+int catalog_exists(FILE * fp, const char *file)
 {
     int i;
     int linenr = 0;
@@ -92,15 +92,17 @@ int catalog_exists(FILE* fp, const char* file)
     char *value;
     char line[1050];
 
+    assert(fp != NULL);
+
     rewind(fp);
 
-    while(!feof(fp))
-    {
+    /* Look for file=<file> */
+    while (!feof(fp)) {
         linenr++;
 
         i = 0;
         fgets(line, 1049, fp);
-        while(i < 1049 && line[i] != '=' && line[i] != '\0')
+        while (i < 1049 && line[i] != '=' && line[i] != '\0')
             i++;
 
         /* malformed line */
@@ -109,8 +111,8 @@ int catalog_exists(FILE* fp, const char* file)
 
         line[i] = '\0';
         op = line;
-        value = line+i+1;
-        value[strlen(value)-1] = '\0';
+        value = line + i + 1;
+        value[strlen(value) - 1] = '\0';
 
         if (strcmp(op, "file") == 0)
             if (strcmp(value, file) == 0)
@@ -120,13 +122,15 @@ int catalog_exists(FILE* fp, const char* file)
     return 0;
 }
 
-void catalog_add(FILE* fp, const char* file)
+void catalog_add(FILE * fp, const char *file)
 {
+    assert(fp != NULL);
+
     if (!catalog_exists(fp, file))
         fprintf(fp, "file=%s\n", file);
 }
 
-void catalog_remove(FILE* fp, const char* file)
+void catalog_remove(FILE * fp, const char *file)
 {
     int i;
     int linenr;
@@ -134,22 +138,25 @@ void catalog_remove(FILE* fp, const char* file)
     char *catalog;
     char *oldcatalog;
     char line[1050];
-    FILE* fpn;
+    FILE *fpn;
 
-    if(catalogpath == NULL)
-        return;
+    assert(fp != NULL);
 
+    /* If we got fp, this must exist */
+    assert(catalogpath != NULL);
+
+    /* if the file is not on the catalog, there's nothing to do */
     linenr = catalog_exists(fp, file);
     if (linenr == 0)
         return;
 
-    catalog = (char *) smalloc((strlen(catalogpath) +
-        strlen("/.index/contents.tmp")) * sizeof(char));
+    /* Open a new catalog */
+    catalog = smalloc((strlen(catalogpath) +
+                       strlen("/.index/contents.tmp")) * sizeof(char));
     sprintf(catalog, "%s/.index/contents.tmp", catalogpath);
     fpn = fopen(catalog, "w+");
 
-    if (fpn == NULL)
-    {
+    if (fpn == NULL) {
         free(catalog);
         return;
     }
@@ -157,15 +164,13 @@ void catalog_remove(FILE* fp, const char* file)
     rewind(fp);
 
     /* copy lines up to the one to remove */
-    for (i=1; i < linenr; i++)
-    {
+    for (i = 1; i < linenr; i++) {
         fgets(line, 1049, fp);
         fprintf(fpn, "%s", line);
     }
 
     /* skip that one */
     fgets(line, 1049, fp);
-
 
     /* copy the rest */
     while (fgets(line, 1049, fp) != NULL)
@@ -175,16 +180,17 @@ void catalog_remove(FILE* fp, const char* file)
 
     /* remove .tmp */
     oldcatalog = strdup(catalog);
-    oldcatalog[strlen(oldcatalog)-4] = '\0';
+    oldcatalog[strlen(oldcatalog) - 4] = '\0';
 
     /* replace the catalog atomically */
     rename(catalog, oldcatalog);
     freopen(oldcatalog, "a+", fp);
+
     free(oldcatalog);
     free(catalog);
 }
 
-char* catalog_hash(FILE* fp, HashTreeNode **copytree, int store)
+char *catalog_hash(FILE * fp, HashTreeNode ** copytree, int store)
 {
     unsigned int i;
     char line[1050];
@@ -193,21 +199,22 @@ char* catalog_hash(FILE* fp, HashTreeNode **copytree, int store)
     char *hash;
     char *catalogpath = catalog_locate();
     char *dump;
-    HashTreeNode* tree = hashtree_new();
-    FILE* fpdump;
+    HashTreeNode *tree = hashtree_new();
+    FILE *fpdump;
 
     if (tree == NULL)
         return NULL;
 
+    /* Change to the repo root so accessing files is easier */
     chdir(catalogpath);
 
     rewind(fp);
 
-    while(!feof(fp))
-    {
+    /* Loop through all of the files on the catalog */
+    while (!feof(fp)) {
         i = 0;
         fgets(line, 1049, fp);
-        while(i < 1049 && line[i] != '=' && line[i] != '\0')
+        while (i < 1049 && line[i] != '=' && line[i] != '\0')
             i++;
 
         /* malformed line */
@@ -216,32 +223,30 @@ char* catalog_hash(FILE* fp, HashTreeNode **copytree, int store)
 
         line[i] = '\0';
         op = line;
-        value = line+i+1;
-        value[strlen(value)-1] = '\0';
+        value = line + i + 1;
+        value[strlen(value) - 1] = '\0';
 
+        /* And append each of them to our new hash tree */
         if (strcmp(op, "file") == 0)
             hashtree_insert(tree, value, NULL);
     }
 
+    /* Compute missing hashes on the tree */
     hash = hashtree_compute(tree);
 
     if (copytree != NULL)
         *copytree = tree;
-    /* else
-        TODO free tree? */
 
+    /* Simply return the hash if we don't need to store the tree */
     if (!store)
         return hash;
 
-    dump = malloc((strlen(catalogpath) + 60) * sizeof(char));
-    if (dump == NULL)
-        return NULL;
-
+    /* Store the tree on a file otherwise */
+    dump = smalloc((strlen(catalogpath) + 60) * sizeof(char));
     sprintf(dump, "%s/.index/hashes/%s", catalogpath, hash);
     fpdump = fopen(dump, "w+");
 
-    if (fpdump == NULL)
-    {
+    if (fpdump == NULL) {
         free(dump);
         free(hash);
         return NULL;
