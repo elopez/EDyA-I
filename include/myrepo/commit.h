f3d7bb1e1b675f2279dbfec655e5ad20e3486b11
Emilio López <elopez93@gmail.com>
2012-12-02 21:09:14 -0300

    commit: cleanup, indent, comment, document and fix small issues

--- include/myrepo/commit.h
+++ include/myrepo/commit.h
@@ -1,14 +1,63 @@
-#include "hashtree.h"
+#include <myrepo/hashtree.h>
 
 #ifndef __H_COMMIT__
 #define __H_COMMIT__
 
+/*
+ * Commits the current repository status for the files included on the
+ * catalog.
+ * @param[int]  message         A user-specified message to associate to the commit
+ * @return 0 on success, 1 on error
+ */
 int myrepo_commit(const char *message);
 
-HashTreeNode * commit_loadtree(const char *catalogpath, unsigned int revision);
+/*
+ * Loads the hash tree associated to a specific commit.
+ * @param[in]   catalogpath     the catalog path
+ * @param[in]   revision        the commit revision to use
+ */
+HashTreeNode *commit_loadtree(const char *catalogpath, unsigned int revision);
+
+/*
+ * Finds out the latest commit number on the repository and, optionally,
+ * increments it by one
+ * @param[in]   catalogpath     the catalog path
+ * @param[in]   increment       increment the commit number by 1 (or not)
+ */
 unsigned int commit_latest(const char *catalogpath, int increment);
-int commit_filestatus(const char *catalogpath, unsigned int rev, const char *file);
-unsigned int commit_file(const char *catalogpath, unsigned int rev, const char *file, char ***fcontents);
-int commit_diff(char *catalogpath, unsigned int rev, const char *file, FILE *fp);
+
+/*
+ * Compares the current file against the one stored on a specific revision
+ * @param[in]   catalogpath     the catalog path
+ * @param[in]   revision        the commit revision to compare against
+ * @param[in]   file            the file to compare
+ * @return 0 if the file was not modified, any other value if it was.
+ */
+int commit_filestatus(const char *catalogpath, unsigned int revision,
+                      const char *file);
+
+/*
+ * Generates a file as it was on a specific revision.
+ * @param[in]   catalogpath     the catalog path
+ * @param[in]   revision        the commit revision to use
+ * @param[in]   file            the file name/path to generate
+ * @param[out]  fcontents       the file contents as stored on that revision
+ * @return the file line length
+ */
+unsigned int commit_file(const char *catalogpath, unsigned int revision,
+                         const char *file, char ***fcontents);
+
+/*
+ * Diffs the current file against the one stored on a specific revision.
+ * Depending on fp value, it will store the diff (commit mode, NULL fp) or
+ * show it via fp
+ * @param[in]   catalogpath     the catalog path
+ * @param[in]   revision        the commit revision to diff against
+ * @param[in]   file            the file to diff
+ * @param[in]   fp              NULL for commit mode, or a FILE * to print to
+ * @return 0 on success, any other value on error
+ */
+int commit_diff(char *catalogpath, unsigned int revision, const char *file,
+                FILE * fp);
 
 #endif
--- myrepo/commit.c
+++ myrepo/commit.c
@@ -18,27 +18,24 @@
 
 int myrepo_commit(const char *message)
 {
-    FILE* catalog;
-    FILE* commit;
-    char * catalogpath = catalog_locate();
-    char * cataloghash;
-    char * revpath;
-    const char ** differences;
-    HashTreeNode * old;
-    HashTreeNode * new;
+    FILE *catalog;
+    FILE *commit;
+    char *catalogpath = catalog_locate();
+    char *cataloghash;
+    char *revpath;
+    const char **differences;
+    HashTreeNode *old;
+    HashTreeNode *new;
     unsigned int revision;
 
-
-    if (catalogpath == NULL)
-    {
+    if (catalogpath == NULL) {
         fprintf(stderr, "You are not inside a repository, aborting.\n");
         return 1;
     }
 
 #ifdef SPEC_COMPLIANT
-    revpath = getcwd(NULL, 0); /* temporal use */
-    if (strcmp(revpath, catalogpath))
-    {
+    revpath = getcwd(NULL, 0);  /* temporal use */
+    if (revpath != NULL && strcmp(revpath, catalogpath)) {
         free(revpath);
         fprintf(stderr, "You are not on the repository root, aborting.\n");
         return 1;
@@ -47,8 +44,7 @@ int myrepo_commit(const char *message)
 #endif
 
     catalog = catalog_open();
-    if (catalog == NULL)
-    {
+    if (catalog == NULL) {
         fprintf(stderr, "Failed to open repository catalog.\n");
         return 1;
     }
@@ -62,39 +58,39 @@ int myrepo_commit(const char *message)
     revision = commit_latest(catalogpath, 1);
 
     /* Store commit message and match version and hash */
-    revpath = (char*) smalloc((strlen(catalogpath) + 100) * sizeof(char));
+    revpath = smalloc((strlen(catalogpath) + 100) * sizeof(char));
     sprintf(revpath, "%s/.index/revs/%d.rev", catalogpath, revision);
     commit = fopen(revpath, "w");
-    if (commit == NULL)
-    {
-        fprintf(stderr, "Failed generating commit file\n");
+    if (commit == NULL) {
+        fprintf(stderr, "Failed to generate commit file.\n");
         return 1;
     }
 
-    if (revision == 1)
-    {
-        old = hashtree_new(); /* empty tree */
+    /* Get the previous tree */
+    if (revision == 1) {
+        old = hashtree_new();   /* empty tree */
         hashtree_compute(old);
     } else {
-        old = commit_loadtree(catalogpath, revision-1);
+        old = commit_loadtree(catalogpath, revision - 1);
     }
 
     /* Compare the trees */
     differences = hashtree_compare(old, new);
 
-    if (differences == NULL)
-    {
+    if (differences == NULL) {
         printf("No changes on this commit.\n");
     } else {
-        for (int i=0; differences[i] != NULL; i++) {
+        /* Indicate which files were involved and generate diffs for them */
+        for (int i = 0; differences[i] != NULL; i++) {
             fprintf(commit, "involved=%s\n", differences[i]);
-            commit_diff(catalogpath, revision-1, differences[i], NULL);
+            commit_diff(catalogpath, revision - 1, differences[i], NULL);
         }
         free(differences);
     }
 
+    /* Print the root hash of the tree, the date, and user message to finish */
     fprintf(commit, "hash=%s\ndate=%u\n%s\n", cataloghash,
-        (unsigned int) time(NULL), message);
+            (unsigned int)time(NULL), message);
     fclose(commit);
 
     free(revpath);
@@ -102,22 +98,22 @@ int myrepo_commit(const char *message)
     return 0;
 }
 
-HashTreeNode * commit_loadtree(const char *catalogpath, unsigned int revision)
+HashTreeNode *commit_loadtree(const char *catalogpath, unsigned int revision)
 {
-    FILE* fd;
-    HashTreeNode* tree;
+    FILE *fd;
+    HashTreeNode *tree;
     char tmphash[50];
-    char* revpath = (char*) smalloc((strlen(catalogpath) + 100) * sizeof(char));
+    char *revpath;
 
     /* Revision 0 is an empty tree */
     if (revision == 0)
-        return hashtree_new();
+        return hashtree_new();  /* TODO? compute? */
 
     /* Find out revision hash */
+    revpath = smalloc((strlen(catalogpath) + 100) * sizeof(char));
     sprintf(revpath, "%s/.index/revs/%d.rev", catalogpath, revision);
     fd = fopen(revpath, "r+");
-    if (fd == NULL)
-    {
+    if (fd == NULL) {
         free(revpath);
         fprintf(stderr, "Failed to figure out old revision hash.\n");
         return NULL;
@@ -128,7 +124,7 @@ HashTreeNode * commit_loadtree(const char *catalogpath, unsigned int revision)
     fclose(fd);
 
     /* And load it, +5 for hash= */
-    sprintf(revpath, "%s/.index/hashes/%s", catalogpath, tmphash+5);
+    sprintf(revpath, "%s/.index/hashes/%s", catalogpath, tmphash + 5);
     tree = hashtree_load(revpath);
 
     free(revpath);
@@ -137,25 +133,24 @@ HashTreeNode * commit_loadtree(const char *catalogpath, unsigned int revision)
 
 unsigned int commit_latest(const char *catalogpath, int increment)
 {
-    FILE* fd;
+    FILE *fd;
     char *revpath;
     unsigned int revision;
-    unsigned int len;
 
-    len = strlen(catalogpath) + 100;
-    revpath = (char*) smalloc(len * sizeof(char));
-    snprintf(revpath, len, "%s/.index/revs/latest", catalogpath);
+    /* Load the latest revision number from storage */
+    revpath = smalloc((strlen(catalogpath) + 100) * sizeof(char));
+    sprintf(revpath, "%s/.index/revs/latest", catalogpath);
     fd = fopen(revpath, "r+");
-    if (fd == NULL)
-    {
-        fprintf(stderr, "Failed to figure out revision number.\n");
+    if (fd == NULL) {
+        fprintf(stderr, "Failed to find out latest revision number.\n");
         free(revpath);
         return 1;
     }
     fscanf(fd, "%u", &revision);
 
+    /* Do we need to increment it? */
     if (increment) {
-        revision++; /* TODO: overflow? */
+        revision++;             /* TODO: overflow? */
         fd = freopen(revpath, "w", fd);
         fprintf(fd, "%u", revision);
     }
@@ -166,17 +161,19 @@ unsigned int commit_latest(const char *catalogpath, int increment)
     return revision;
 }
 
-int commit_filestatus(const char *catalogpath, unsigned int rev, const char *file)
+int commit_filestatus(const char *catalogpath, unsigned int revision,
+                      const char *file)
 {
-    FILE* catalog;
+    FILE *catalog;
     static HashTreeNode *new = NULL;
     static HashTreeNode *old = NULL;
+    static unsigned int oldrev = 0;
     const char *oldhash;
 
+    /* Hash the current tree once */
     if (new == NULL) {
         catalog = catalog_open();
-        if (catalog == NULL)
-        {
+        if (catalog == NULL) {
             fprintf(stderr, "Failed to open repository catalog.\n");
             return 1;
         }
@@ -184,56 +181,62 @@ int commit_filestatus(const char *catalogpath, unsigned int rev, const char *fil
         fclose(catalog);
     }
 
-    if (old == NULL)
-        old = commit_loadtree(catalogpath, rev);
+    /* Load the old tree once, if revisions match */
+    if (old == NULL && oldrev != revision) {
+        old = commit_loadtree(catalogpath, revision);
+        oldrev = revision;
+    }
 
+    /* Fetch the hash from the old tree */
     oldhash = hashtree_fetch(old, file);
 
-    /* 0: up to date !0: modified */
-    if (oldhash == NULL) /* freshly-added file, therefore modified */
+    if (oldhash == NULL)        /* freshly-added file, therefore modified */
         return 1;
 
+    /* 0: up to date !0: modified */
     return strcmp(oldhash, hashtree_fetch(new, file));
 }
 
-static int commit_file_is_involved(const char *catalogpath, unsigned int rev, const char *file)
+static int commit_file_is_involved(const char *catalogpath, unsigned int rev,
+                                   const char *file)
 {
-    FILE* fd;
+    FILE *fd;
     char tmpread[50];
-    char* revpath;
+    char *revpath;
 
     /* No files are involved on revision 0 */
     if (rev == 0)
         return 0;
 
-    revpath = (char*) smalloc((strlen(catalogpath) + 100) * sizeof(char));
+    revpath = smalloc((strlen(catalogpath) + 100) * sizeof(char));
 
     /* Find out revision hash */
     sprintf(revpath, "%s/.index/revs/%d.rev", catalogpath, rev);
     fd = fopen(revpath, "r+");
-    if (fd == NULL)
-    {
+    if (fd == NULL) {
         fprintf(stderr, "Failed to open revision data.\n");
         free(revpath);
         return 0;
     }
 
+    /* See if file is on any of the involved= lines */
     do {
         fgets(tmpread, 46, fd);
     } while (!feof(fd) && strncmp("involved=", tmpread, 9) != 0);
 
     while (!feof(fd) && strncmp("involved=", tmpread, 9) == 0
-        && strncmp(file, tmpread+9, strlen(file)) != 0) {
+           && strncmp(file, tmpread + 9, strlen(file)) != 0) {
         fgets(tmpread, 46, fd);
     }
 
     fclose(fd);
     free(revpath);
 
-    return !strncmp(file, tmpread+9, strlen(file));
+    return !strncmp(file, tmpread + 9, strlen(file));
 }
 
-unsigned int commit_file(const char *catalogpath, unsigned int rev, const char *file, char ***fcontents)
+unsigned int commit_file(const char *catalogpath, unsigned int revision,
+                         const char *file, char ***fcontents)
 {
     FILE *fp;
     unsigned int i;
@@ -245,23 +248,22 @@ unsigned int commit_file(const char *catalogpath, unsigned int rev, const char *
     unsigned int filepatchlen;
     char *path;
 
-    fp = fopen("/dev/null", "r"); /* TODO */
+    fp = fopen("/dev/null", "r");   /* TODO */
     fileold = readfile(fp, &fileoldlen);
     filenew = fileold;
     filenewlen = fileoldlen;
     fclose(fp);
 
-    path = (char*) smalloc((strlen(catalogpath)+strlen(file)+100)*sizeof(char));
+    path = smalloc((strlen(catalogpath) + strlen(file) + 100) * sizeof(char));
 
-    for (i = 1; i <= rev; i++)
-    {
-        if(commit_file_is_involved(catalogpath, i, file))
-        {
-            sprintf(path, "%s/.index/patches/%u/%s.patch", catalogpath, i, file+2);
+    /* Go from rev 1 to revision, and if file is involved in it, patch ours */
+    for (i = 1; i <= revision; i++) {
+        if (commit_file_is_involved(catalogpath, i, file)) {
+            sprintf(path, "%s/.index/patches/%u/%s.patch", catalogpath, i,
+                    file + 2);
             fp = fopen(path, "r");
-            filepatch = readfile(fp, &filepatchlen); /* TODO */
-            patch_file(fileold, fileoldlen, filepatch, filepatchlen,
-                &filenew, &filenewlen); /* TODO! */
+            filepatch = readfile(fp, &filepatchlen);    /* TODO */
+            patch_file(fileold, fileoldlen, filepatch, filepatchlen, &filenew, &filenewlen);    /* TODO! */
 
             fclose(fp);
             freereadfile(filepatch);
@@ -278,7 +280,8 @@ unsigned int commit_file(const char *catalogpath, unsigned int rev, const char *
     return filenewlen;
 }
 
-int commit_diff(char *catalogpath, unsigned int rev, const char *file, FILE *fp)
+int commit_diff(char *catalogpath, unsigned int revision, const char *file,
+                FILE * fp)
 {
     FILE *fcurrent;
     FILE *diff;
@@ -290,20 +293,23 @@ int commit_diff(char *catalogpath, unsigned int rev, const char *file, FILE *fp)
     unsigned int fcurrlen;
     struct rule *rules;
 
+    /* fp == NULL means we are in commit mode */
     if (fp == NULL) {
-        path = (char*) smalloc((strlen(catalogpath)+strlen(file)+100)*sizeof(char));
-        sprintf(path, "%s/.index/patches/%u/%s.patch", catalogpath, rev+1, file+2);
-        mkpath(path, 0770); /* TODO */
-        diff = fopen(path, "w"); /* TODO */
+        path = smalloc((strlen(catalogpath) + strlen(file) + 100) *
+                       sizeof(char));
+        sprintf(path, "%s/.index/patches/%u/%s.patch", catalogpath,
+                revision + 1, file + 2);
+        mkpath(path, 0770);     /* TODO */
+        diff = fopen(path, "w");    /* TODO */
         free(path);
-    } else {
+    } else {                    /* fp != NULL means display mode */
         diff = fp;
     }
 
-    flen = commit_file(catalogpath, rev, file, &fcontents);
+    flen = commit_file(catalogpath, revision, file, &fcontents);
     fcurrent = fopen(file, "r");
-    if (fcurrent == NULL && fp == NULL) /* TODO use stat? */
-    {
+    if (fcurrent == NULL && fp == NULL) {   /* TODO use stat? */
+        /* implicit myrepo rm: file is not there anymore, remove from catalog */
         printf("Dropping deleted file from catalog: %s\n", file);
         fcurrent = catalog_open();
         catalog_remove(fcurrent, file);
@@ -311,9 +317,13 @@ int commit_diff(char *catalogpath, unsigned int rev, const char *file, FILE *fp)
         fclose(fcurrent);
         return 0;
     }
+
+    /* TODO: what happens on display mode when file doesn't exist? */
+
     fcurrcontents = readfile(fcurrent, &fcurrlen);
     fclose(fcurrent);
 
+    /* Diff file@revision and current file */
     status = diff_lines(&rules, fcontents, flen, fcurrcontents, fcurrlen);
 
     assert(status != DIFF_ERROR);
