b5bedd87e2403e753fe73a24407941a7fb1fcad8
Emilio López <elopez93@gmail.com>
2012-12-04 02:22:03 -0300

    usage: indent and document

--- include/myrepo/usage.h
+++ include/myrepo/usage.h
@@ -1,6 +1,11 @@
 #ifndef __H_USAGE__
 #define __H_USAGE__
 
+/**
+ * Prints the usage help for myrepo
+ * @param[in]       name    the executable name, usually "myrepo"
+ * @return 1
+ */
 int myrepo_usage(char *name);
 
 #endif
--- myrepo/usage.c
+++ myrepo/usage.c
@@ -5,12 +5,12 @@
 int myrepo_usage(char *name)
 {
     fprintf(stderr, "Usage: %s \n"
-                    " -  init\n"
-                    " -  add <file/directory> (<file/directory> ..)\n"
-                    " -  rm <file/directory> (<file/directory> ..)\n"
-                    " -  status\n"
-                    " -  commit -m \"commit message\"\n"
-                    " -  log (<revision>)\n"
-                    " -  compare <path/to/repo1> <path/to/repo2>\n", name);
+            " -  init\n"
+            " -  add <file/directory> (<file/directory> ..)\n"
+            " -  rm <file/directory> (<file/directory> ..)\n"
+            " -  status\n"
+            " -  commit -m \"commit message\"\n"
+            " -  log (<revision>)\n"
+            " -  compare <path/to/repo1> <path/to/repo2>\n", name);
     return 1;
 }
