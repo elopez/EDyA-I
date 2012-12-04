922f2362b2806354618d014ad1c3e2f2a6f9c91d
Emilio López <elopez93@gmail.com>
2012-12-04 03:11:39 -0300

    recursive: indent, document and apply a small optimization.

--- include/myrepo/recursive.h
+++ include/myrepo/recursive.h
@@ -3,13 +3,42 @@
 #ifndef __H_ADDRM__
 #define __H_ADDRM__
 
-typedef void (*callback)(char* file, void* extra);
+/**
+ * Callback type for myrepo_recursive.
+ * @param[in]   file        the filename to be used
+ * @param[in]   extra       an extra pointer for any needed external information
+ */
+typedef void (*callback) (char *file, void *extra);
 
-int myrepo_recursive(char **filename, callback function, void* extra);
+/**
+ * Callback-typed function to add a file to the catalog.
+ * @param[in]   name        the file name to add to the catalog
+ * @param[in]   catalog     a FILE * to the catalog
+ */
+void myrepo_add(char *name, void *catalog);
 
-/* Wrappers to catalog */
-void myrepo_add(char*, void*);
-void myrepo_remove(char*, void*);
-void myrepo_untracked(char *, void *);
+/**
+ * Callback-typed function to remove a file from the catalog.
+ * @param[in]   name        the file name to remove from the catalog
+ * @param[in]   catalog     a FILE * to the catalog
+ */
+void myrepo_remove(char *name, void *catalog);
+
+/**
+ * Callback-typed function to print the current status of a file
+ * @param[in]   name        the file name to analyze
+ * @param[in]   catalog     a FILE * to the catalog
+ */
+void myrepo_untracked(char *name, void *catalog);
+
+/**
+ * Run a callback function on all the files listed on filename and
+ * its children if applicable.
+ * @param[in]   filename    a NULL-terminated array of paths
+ * @param[in]   function    the callback to execute
+ * @param[in]   extra       an optional pointer handed to the callback
+ * @return 0 on success, any other value on error
+ */
+int myrepo_recursive(char **filename, callback function, void *extra);
 
 #endif
--- myrepo/recursive.c
+++ myrepo/recursive.c
@@ -13,7 +13,8 @@
 #include <myrepo/commit.h>
 #include <myrepo/recursive.h>
 
-static int myrepo_recursive_step2(char *directory, callback function, void* extra)
+static int myrepo_recursive_step2(char *directory, callback function,
+                                  void *extra)
 {
     char *name[2];
     int dirlen = strlen(directory);
@@ -21,27 +22,23 @@ static int myrepo_recursive_step2(char *directory, callback function, void* extr
     struct dirent *de;
 
     /* remove trailing / on directory names */
-    while (dirlen > 1 && directory[dirlen-1] == '/')
-    {
-        directory[dirlen-1] = '\0';
+    while (dirlen > 1 && directory[dirlen - 1] == '/') {
+        directory[dirlen - 1] = '\0';
         dirlen--;
     }
 
-    if ((dir = opendir(directory)) == NULL)
-    {
+    if ((dir = opendir(directory)) == NULL) {
         fprintf(stderr, "%s: error opening directory\n", directory);
         return 1;
     }
 
-    while((de = readdir(dir)) != NULL)
-    {
+    while ((de = readdir(dir)) != NULL) {
         if (strcmp(".", de->d_name) == 0 ||
-            strcmp("..", de->d_name) == 0 ||
-            strcmp(".index", de->d_name) == 0)
+            strcmp("..", de->d_name) == 0 || strcmp(".index", de->d_name) == 0)
             continue;
 
-        name[0] = (char *) smalloc((strlen(de->d_name) +
-            dirlen + 4) * sizeof(char));
+        name[0] = (char *)smalloc((strlen(de->d_name) +
+                                   dirlen + 4) * sizeof(char));
         sprintf(name[0], "%s/%s", directory, de->d_name);
         name[1] = NULL;
 
@@ -56,13 +53,14 @@ static int myrepo_recursive_step2(char *directory, callback function, void* extr
     return 0;
 }
 
-int myrepo_recursive(char **filename, callback function, void* extra)
+int myrepo_recursive(char **filename, callback function, void *extra)
 {
     struct stat st;
-    FILE* catalog;
-    char* fname;
-    char* cwd;
-    char* relative;
+    char *fname;
+    char *cwd;
+    char *relative;
+    FILE *catalog;
+    static char *catalogpath;
 
     /* filename is a null terminated array of strings, containing the
      * file names we should add to our index */
@@ -70,15 +68,16 @@ int myrepo_recursive(char **filename, callback function, void* extra)
     /* the filename array must always contain at least 1 filename */
     assert(*filename != NULL);
 
-    if (catalog_locate() == NULL)
-    {
-        fprintf(stderr, "You are not inside a repository, aborting.\n");
-        return 1;
+    if (catalogpath == NULL) {
+        catalogpath = catalog_locate();
+        if (catalogpath == NULL) {
+            fprintf(stderr, "You are not inside a repository, aborting.\n");
+            return 1;
+        }
     }
 
     catalog = catalog_open();
-    if (catalog == NULL)
-    {
+    if (catalog == NULL) {
         fprintf(stderr, "Failed to open repository catalog.\n");
         return 1;
     }
@@ -88,31 +87,30 @@ int myrepo_recursive(char **filename, callback function, void* extra)
 
     /* Where are we? */
     cwd = getcwd(NULL, 0);
-    relative = cwd + strlen(catalog_locate());
-    chdir(catalog_locate());
+    relative = cwd + strlen(catalogpath);
+    chdir(catalogpath);
 
     do {
-        fname = (char *) smalloc((strlen(*filename) + strlen(relative) + 2) * sizeof(char));
+        fname = smalloc((strlen(*filename) + strlen(relative) + 2) *
+                        sizeof(char));
         if (fname == NULL)
             continue;
 
         /* always use ./file */
-        if((*filename)[0] == '.' && (*filename)[1] == '\0')
+        if ((*filename)[0] == '.' && (*filename)[1] == '\0')
             sprintf(fname, ".%s", relative);
-        else if((*filename)[0] == '.' && (*filename)[1] == '/')
+        else if ((*filename)[0] == '.' && (*filename)[1] == '/')
             sprintf(fname, ".%s/%s", relative, *filename + 2);
         else
             sprintf(fname, ".%s/%s", relative, *filename);
 
-        if(stat(fname, &st) == 0)
-        {
-            if(S_ISDIR(st.st_mode))
+        if (stat(fname, &st) == 0) {
+            if (S_ISDIR(st.st_mode))
                 myrepo_recursive_step2(fname, function, extra);
-            else if(S_ISREG(st.st_mode))
+            else if (S_ISREG(st.st_mode))
                 function(fname, extra);
             else {
-                fprintf(stderr, "%s: not a regular file or directory\n",
-                    fname);
+                fprintf(stderr, "%s: not a regular file or directory\n", fname);
                 free(cwd);
                 free(fname);
                 return 1;
@@ -135,27 +133,27 @@ int myrepo_recursive(char **filename, callback function, void* extra)
     return 0;
 }
 
-void myrepo_add(char* name, void* extra)
+void myrepo_add(char *name, void *extra)
 {
-    catalog_add((FILE*)extra, name);
+    catalog_add((FILE *) extra, name);
 }
 
-void myrepo_remove(char* name, void* extra)
+void myrepo_remove(char *name, void *extra)
 {
-    catalog_remove((FILE*)extra, name);
+    catalog_remove((FILE *) extra, name);
 }
 
-void myrepo_untracked(char* name, void* extra)
+void myrepo_untracked(char *name, void *extra)
 {
     static char *catalogpath = NULL;
     static unsigned int latest;
 
-    if(catalogpath == NULL) {
+    if (catalogpath == NULL) {
         catalogpath = catalog_locate();
         latest = commit_latest(catalogpath, 0);
     }
 
-    if (!catalog_exists((FILE*)extra, name))
+    if (!catalog_exists((FILE *) extra, name))
         printf("Untracked:\t%s\n", name);
     else if (commit_filestatus(catalogpath, latest, name))
         printf("Modified:\t%s\n", name);
