60960d994029632e999c5030d732ac48c73f80e7
Emilio López <elopez93@gmail.com>
2012-12-08 00:01:08 -0300

    pager: improve documentation

--- include/shared/pager.h
+++ include/shared/pager.h
@@ -6,12 +6,14 @@
 /**
  * Opens a stream similar to stdout, but paged using an available
  * system pager. In case no pager is available, it defaults to stdout.
+ * @return a valid FILE * to output to
  */
 FILE *pager_init(void);
 
 /**
  * Closes a pager stream, if necessary.
  * @param[in]   fp  The pager stream to close.
+ * @return 0 on success, any other value on error
  */
 int pager_close(FILE * fp);
 
