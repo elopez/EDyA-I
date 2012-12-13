8ee2d4a6ebcd5cf13e0de92507f4bcb2f37a5d37
Emilio López <elopez93@gmail.com>
2012-12-13 17:59:39 -0300

    patch: indent, clean and document

--- include/shared/patch.h
+++ include/shared/patch.h
@@ -3,9 +3,22 @@
 #ifndef __H_PATCH__
 #define __H_PATCH__
 
+/* Return codes for patch_file */
 #define PATCH_OK    (1 << 0)
 #define PATCH_ERROR (1 << 1)
 
-int patch_file(char **, unsigned int, char **, unsigned int, char ***, unsigned int *);
-
+/**
+ * Patches a file with a text patch
+ * @param[in]   fileold         The origin file
+ * @param[in]   filelen         The origin file's length
+ * @param[in]   patch           The patch file
+ * @param[in]   patchlen        The patch file's length
+ * @param[out]  newfilep        A pointer to store the new file in
+ * @param[out]  newlen          Optional, a pointer to store newfilep's length
+ * @return PATCH_OK on success
+ *         PATCH_ERROR if an error occured
+ */
+int patch_file(char **fileold, unsigned int filelen,
+               char **patch, unsigned int patchlen,
+               char ***newfilep, unsigned int *newlen);
 #endif
--- shared/patch.c
+++ shared/patch.c
@@ -10,31 +10,30 @@
 #define HUNK_APPEND (1 << 2)
 #define HUNK_REMOVE (1 << 3)
 
-int patch_file(char ** fileold, unsigned int filelen,
-                char ** patch, unsigned int patchlen,
-                char *** newfilep, unsigned int *newlen)
+int patch_file(char **fileold, unsigned int filelen,
+               char **patch, unsigned int patchlen,
+               char ***newfilep, unsigned int *newlen)
 {
     unsigned int i, j;
     unsigned int hunktype = HUNK_NONE;
     unsigned int oldstart, oldend, newstart, newend, count = 0;
 
-    char ** file = scalloc(filelen+1, sizeof(char *));
-    if(filelen > 0)
+    char **file = scalloc(filelen + 1, sizeof(char *));
+    if (filelen > 0)
         memcpy(file, fileold, sizeof(char *) * filelen);
-    char ** newfile = scalloc(filelen + patchlen + 1, sizeof(char *));
+    char **newfile = scalloc(filelen + patchlen + 1, sizeof(char *));
     *newfilep = newfile;
 
     /* Parse patch file */
-    for (i=0; i < patchlen; i++)
-    {
+    for (i = 0; i < patchlen; i++) {
         /* Hunk start */
-        if (patch[i][0] >= '0' && patch[i][0] <= '9')
-        {
+        if (patch[i][0] >= '0' && patch[i][0] <= '9') {
             oldstart = atoi(patch[i]);
             j = 1;
             while (patch[i][j] != ',' && patch[i][j] != '\0' &&
-                    patch[i][j] != 'a' && patch[i][j] != 'c' &&
-                    patch[i][j] != 'd') j++;
+                   patch[i][j] != 'a' && patch[i][j] != 'c' &&
+                   patch[i][j] != 'd')
+                j++;
 
             /* Malformed patch */
             if (patch[i][j] == '\0')
@@ -48,7 +47,8 @@ int patch_file(char ** fileold, unsigned int filelen,
                 oldend = oldstart;
 
             while (patch[i][j] != '\0' && patch[i][j] != 'a' &&
-                    patch[i][j] != 'c' && patch[i][j] != 'd') j++;
+                   patch[i][j] != 'c' && patch[i][j] != 'd')
+                j++;
 
             if (patch[i][j] == 'a')
                 hunktype = HUNK_APPEND;
@@ -61,7 +61,8 @@ int patch_file(char ** fileold, unsigned int filelen,
 
             newstart = atoi(patch[i] + j + 1);
 
-            while (patch[i][j] != ',' && patch[i][j] != '\0') j++;
+            while (patch[i][j] != ',' && patch[i][j] != '\0')
+                j++;
 
             /* No comma, single digit */
             if (patch[i][j] == '\0')
@@ -74,9 +75,11 @@ int patch_file(char ** fileold, unsigned int filelen,
                 return PATCH_ERROR;
 
             continue;
+
         /* Fast forward over the patch */
         } else if (hunktype == HUNK_NONE) {
             continue;
+
         /* Old lines */
         } else if (patch[i][0] == '<') {
             /* This is a *really* simple patch implementation, so
@@ -85,45 +88,49 @@ int patch_file(char ** fileold, unsigned int filelen,
              * make sure it's deleting the correct ones. */
             if (hunktype == HUNK_REMOVE || hunktype == HUNK_CHANGE)
                 for (j = oldstart; j <= oldend; j++)
-                    file[j-1] = (char *) 0xDEADBEEF; /* TODO: free line? */
+                    file[j - 1] = (char *)0xDEADBEEF;
 
             /* Don't run this loop again */
             oldstart = oldend + 1;
             continue;
+
         /* New lines */
         } else if (patch[i][0] == '>') {
             if (newstart <= newend) {
-                newfile[newstart-1] = smalloc(strlen(patch[i]) * sizeof(char)); /* TODO */
-                sprintf(newfile[newstart-1], "%s", patch[i] + 2);
+                newfile[newstart - 1] = smalloc(strlen(patch[i]) * sizeof(char));
+                sprintf(newfile[newstart - 1], "%s", patch[i] + 2);
                 newstart++;
             }
+
         /* Switch of hunk */
         } else if (patch[i][0] == '-') {
             continue;
+
         /* Empty tailing line */
         } else if (patch[i][0] == '\r' || patch[i][0] == '\n') {
             continue;
+
         /* End of patch */
         } else if (patch[i][0] == '\0') {
             break;
+
+        /* Malformed patch */
         } else {
-            /* Malformed patch */
             return PATCH_ERROR;
         }
     }
 
     i = j = 0;
     /* Copy the not changed lines */
-    while (file[i] != NULL && filelen--)
-    {
+    while (file[i] != NULL && filelen--) {
         /* if it's a deleted line, skip it */
-        if (file[i] == (char *) 0xDEADBEEF) {
+        if (file[i] == (char *)0xDEADBEEF) {
             i++;
             continue;
         }
 
         /* find the next empty slot */
-        while(newfile[j] != NULL)
+        while (newfile[j] != NULL)
             j++;
 
         /* Move the line */
@@ -131,8 +138,8 @@ int patch_file(char ** fileold, unsigned int filelen,
         i++;
     }
 
-    if(newlen != NULL) {
-        while(newfile[count] != NULL)
+    if (newlen != NULL) {
+        while (newfile[count] != NULL)
             count++;
         *newlen = count;
     }
