bc9f2ba77c19e0417c460f14cbb2adc7c77c5646
Emilio López <elopez93@gmail.com>
2012-12-14 02:04:50 -0300

    Document myrepo_lock/unlock

--- include/myrepo/lock.h
+++ include/myrepo/lock.h
@@ -1,7 +1,18 @@
 #ifndef __H_LOCK__
 #define __H_LOCK__
 
+/**
+ * Prevents other instances of myrepo from running and creates a flag file
+ * for repository integrity check on future runs.
+ * @return 0 if an error occured, any other value on success.
+ */
 int myrepo_lock(void);
+
+/**
+ * Unlinks the integrity check flag file. Intended to be run on normal exit
+ * via atexit(). The myrepo lock will be automatically unlocked when the
+ * process exits.
+ */
 void myrepo_unlock(void);
 
 #endif
